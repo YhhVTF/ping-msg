a228cd76b236a755a36d4026b860c90c91b2d815
George <Georgeskinner15@icloud.com>
2026-07-20 06:26:15 +0100

    Add icon to window

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "fyne.io/fyne/v2"
-    "fyne.io/fyne/v2/app"
-    "fyne.io/fyne/v2/container"
-    "fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 
-    "os"
+	"os"
 )
 
 // Whether or not the user has quit Ping
@@ -14,29 +14,35 @@ var PingQuit = false
 
 // StartPing: Wrapper function that initializes the main window and gui and then connects to the server by calling InitGUI and StartNet respectively
 // Parameters:
-//  a (fyne.App) - argument for InitGUI
-//  loadingWindow (fyne.Window) - argument for InitGUI
+//
+//	a (fyne.App) - argument for InitGUI
+//	loadingWindow (fyne.Window) - argument for InitGUI
 func StartPing(a fyne.App, loadingWindow fyne.Window) {
-    StartNet(InitGUI(a, loadingWindow))
+	StartNet(InitGUI(a, loadingWindow))
 }
 
 func main() {
-    InitLog(os.Stderr, os.Stdout, os.Stdout)
-    Info.Printf("Starting Ping\n")
+	InitLog(os.Stderr, os.Stdout, os.Stdout)
+	Info.Printf("Starting Ping\n")
 
-    a := app.New()
+	a := app.New()
 
-    // Create a loading window
-    // This window will be shown while Ping starts and will be closed when it is ready
-    loadingWindow := a.NewWindow("Launching Ping")
-    c := container.NewCenter(widget.NewLabel("Loading..."))
-    fyne.Do(func() {
-        loadingWindow.SetContent(c)
-    })
-    fyne.Do(loadingWindow.Show)
-    //fyne.Do(loadingWindow.SetMaster)
+	iconData, err := os.ReadFile("assets/icon.png")
+	if err == nil {
+		a.SetIcon(fyne.NewStaticResource("icon.png", iconData))
+	}
 
-    go StartPing(a, loadingWindow)
+	// Create a loading window
+	// This window will be shown while Ping starts and will be closed when it is ready
+	loadingWindow := a.NewWindow("Launching Ping")
+	c := container.NewCenter(widget.NewLabel("Loading..."))
+	fyne.Do(func() {
+		loadingWindow.SetContent(c)
+	})
+	fyne.Do(loadingWindow.Show)
+	//fyne.Do(loadingWindow.SetMaster)
 
-    a.Run()
+	go StartPing(a, loadingWindow)
+
+	a.Run()
 }
