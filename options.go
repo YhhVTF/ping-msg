ff41c4be3f1c552286fde7795076d26dc535e30b
YhhVTF <218009461+YhhVTF@users.noreply.github.com>
2026-07-26 11:13:16 -0400

    finished the option structs

--- options.go
+++ options.go
@@ -1,10 +1,32 @@
 package main
 
 type GUIOptions struct {
-    Window WidgetOptions   `json:"window"`
+    ButtonAttach        ButtonOptions   `json:"button_attach"`
+    ButtonOptions       ButtonOptions   `json:"button_options"`
+    ButtonSend          ButtonOptions   `json:"button_send"`
+    DialogConnIssues    DialogOptions   `json:"dialog_connection_issues"`
+    DialogLogin         DialogOptions   `json:"dialog_login"`
+    EntryMessage        EntryOptions    `json:"entry_message"`
+    EntryUsername       EntryOptions    `json:"entry_username"`
+    Window              WindowOptions   `json:"window"`
 }
 
-type WidgetOptions struct {
+type ButtonOptions struct {
     Label   string  `json:"label"`
+}
+
+type DialogOptions struct {
+    Buttons []ButtonOptions `json:"buttons"`
+    Prompt  string          `json:"prompt"`
+    Size    []int           `json:"size"`
+    Title   string          `json:"title"`
+}
+
+type EntryOptions struct {
+    Label   string  `json:"label"`
+}
+
+type WindowOptions struct {
     Size    []int   `json:"size"`
+    Title   string  `json:"title"`
 }
