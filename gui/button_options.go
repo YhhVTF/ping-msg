b9e1fd2b1abd1f802d68679a1a2b9286c6d60342
YhhVTF <218009461+YhhVTF@users.noreply.github.com>
2026-07-28 00:25:05 -0400

    tried to add an options window but yh

--- gui/button_options.go
+++ gui/button_options.go
@@ -1,6 +1,8 @@
 package gui
 
 import (
+    "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/container"
     "fyne.io/fyne/v2/widget"
 
     "github.com/YhhVTF/ping-msg/log"
@@ -12,6 +14,10 @@ func createButtonOptions(g *GUI, opt *options.Options) *widget.Button {
     button := widget.NewButton(opt.GUIText.ButtonOptions.Label, func(){
         log.Info.Printf("Widget ButtonOptions pressed\n")
         defer g.Window.Canvas().Focus(g.Widgets.EntryMessage)
+
+        w := container.NewInnerWindow("Options", widget.NewButton("e", func(){}))
+        w.Resize(fyne.NewSize(500, 600))
+        w.SetMaximized(false)
     })
     return button
 }
