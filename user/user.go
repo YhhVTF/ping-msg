11ea7df9f17d32422a4b7deb4f853ccd3c087625
YhhVTF <218009461+YhhVTF@users.noreply.github.com>
2026-07-30 11:56:16 -0400

    added user id to UserData (now UserCache) along with other data that would be attributed to users

--- user/user.go
+++ user/user.go
@@ -1,9 +1,25 @@
 package user
 
+// Data of a user
+type User struct {
+    Bio         string
+    ID          int
+    Pfp         []byte
+    Username    string
+}
+
 // Data about the client's user and other user data received from the server
-type UserData struct {
+type UserCache struct {
+    // The bio of this client's user
+    ThisBio         string
+    // The profile picture of this client's user
+    ThisPfp         []byte
+    // The user ID of this client's user
+    ThisUserID      int
     // The username of this client's user
-    ThisUser string
-    // -
-    Users map[int]string
+    ThisUsername    string
+    // Cache of other users' data
+    //  Key (int) - User ID
+    //  Val (User) - User data
+    Users           map[int]User
 }
