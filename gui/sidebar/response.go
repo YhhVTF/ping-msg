6f14a6ca9f6e98900ae6134941762efabd265435
YhhVTF <218009461+YhhVTF@users.noreply.github.com>
2026-08-27 00:43:34 -0400

    added file for handling protocol responses affecting the sidebar

---
+++ gui/sidebar/response.go
@@ -0,0 +1,11 @@
+package sside
+
+import (
+    "github.com/YhhVTF/ping-msg/chat"
+    "github.com/YhhVTF/ping-msg/protocol"
+)
+
+func (g *ScreenSidebar) ChatMetadataRespGet(
+    resp prot.ChatMetadataResponse, c *chat.ChatCache,
+) {
+}
