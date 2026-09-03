a6ed4c1359caad0f5e3697d092dfe0457eaf6e13
YhhVTF <218009461+YhhVTF@users.noreply.github.com>
2026-09-03 01:31:30 -0400

    net.serverSend now accepts outgoing ChatMetadataReuest's from *ScreenSidebar

--- net/net.go
+++ net/net.go
@@ -209,13 +209,20 @@ func serverSend(conn net.Conn, gui *gui.GUI, done <-chan struct{}, signalDone fu
 	encoder := json.NewEncoder(conn)
 	for {
 		select {
-		case req := <-gui.Chat.OutgoingRequests:
-			log.Info.Printf("Sending %s request to server\n", req.Type)
+		case req := <-gui.OutgoingRequests:
 			if err := encoder.Encode(req); err != nil {
-				log.Error.Printf("Failed to send outgoing request: %s\n", err)
+				log.Error.Printf("Failed to send outgoing request %s: %s\n", req.Type, err)
 				signalDone()
 				return
 			}
+            log.Info.Printf("Sent request %s to server\n", req.Type)
+        case req := <-gui.Sidebar.OutgoingReqChatMetadata:
+            if err := encoder.Encode(req); err != nil {
+                log.Error.Printf("Failed to send outgoing request %s: %s\n", req.Type, err)
+                signalDone()
+                return
+            }
+            log.Info.Printf("Sent request %s to server\n", req.Type)
 		case <-done:
 			return
 		}
